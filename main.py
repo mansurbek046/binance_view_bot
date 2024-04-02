8d66d2bb2c47319f9a953532a9960f5e85f750db
mansurbek046 <mansurbek046@gmail.com>
2024-04-02 11:46:31 +0500

    update main.py

--- main.py
+++ main.py
@@ -213,7 +213,7 @@ async def handler(client,message):
   try:
     member=await client.get_chat_member(CHANNEL_ID, message.from_user.id)
     
-    if not member.status!="ChatMemberStatus.MEMBER":
+    if not str(member.status)!="ChatMemberStatus.MEMBER":
       await message.reply_text("Please join our channel and /start again to use bot.")
     else:
       message_text=message.text
@@ -250,8 +250,8 @@ async def handler(client,message):
               status_icon=down
     
             gift_text = f"<b>{symbol} Market\n\n💰 Price: {price:,.2f} USDT\n{status_icon} 24H Change: {change_percentage:.2f}%\n⬆ High: {high_price:,.2f} USDT\n⬇️ Low: {low_price:,.2f} USDT\n📊 24H Vol: {volume:,.2f} USDT</a></b>\n\n<a href='https://www.binance.com/en/trade/{currency}'>💰 Binance</a> | <a href='https://www.tradingview.com/symbols/{currency}'>TradingView 📊</a>"
-            print(message.chat.type)
-            if message.chat.type=="ChatType.GROUP":
+            print(type(message.chat.type))
+            if str(message.chat.type)=="ChatType.GROUP":
               print("GROUP")
               var_get_add=await get_add(client) or ""
               await client.send_message(chat_id=message.chat.id,text=f"{gift_text}{var_get_add}")
