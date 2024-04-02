d1fffee07d1069d82eeb2590b70c03b3fbe994db
mansurbek046 <mansurbek046@gmail.com>
2024-04-02 12:26:41 +0500

    update main.py

--- main.py
+++ main.py
@@ -206,7 +206,10 @@ async def send(client,message):
     message_text=message.text.split("/send ")
     for user_id in data:
       await client.send_message(chat_id=user_id,text=message_text[1])
-    
+
+@app.on_message(filters.command('send'))
+async def donate(cluent,message):
+  cluent.send_message(chat_id=message.chat.id,text="<b>Donate with:</b>\n\n💎 TON:\n`UQDcKDC6iPEosN5e8PRHp7gXxK2KnEgMZjWj8Wyazhvfh8eD`\n💰 USDT:\n`TM2DEqLhBZFuCv1NSx9ftoGESUPvhaSe6Q`\n🪙 BTC:\n`1BDtumr5qNsgvauwG8NSkmkGD9Hxuxt3br`") 
 
 @app.on_message()
 async def handler(client,message):
